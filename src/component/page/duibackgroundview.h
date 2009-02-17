298e242f9085cf7aec78c2e842d48b59dee78cc4
rinyu <ext-laszlo.rinyu@nokia.com>
2009-02-17 10:13:47 -0600

    Added: background png to PannableViewport.

---
+++ src/component/page/duibackgroundview.h
@@ -0,0 +1,29 @@
+#ifndef DUIBACKGROUNDVIEW_H
+#define DUIBACKGROUNDVIEW_H
+
+#include <duiwidgetview.h>
+#include <duistylable.h>
+#include <duipannableviewport.h>
+
+class DuiBackgroundView : public DuiStylable<DuiBackgroundView, DuiWidgetView> 
+{
+	public:
+		DuiBackgroundView(DuiPannableViewport &widget);
+
+		virtual void paint(QPainter *painter, 
+				   const QStyleOptionGraphicsItem *option, 
+				   QWidget* widget);
+
+		virtual void setGeometry(const QRectF &rect);
+	        virtual QRectF boundingRect() const;
+
+		static void registerStyleAttributes(DuiStyleDescription &description);
+
+	protected:
+		virtual QSizeF sizeHint(Qt::SizeHint which, 
+				        const QSizeF & constraint = QSizeF()) const;
+
+	private:
+		DuiPannableViewport &m_viewport;	
+};
+#endif // DUIBACKGROUNDVIEW_H
