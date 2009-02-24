8a0d9d225e4d597e86dba8cfd2f33c578e32048a
mihaly.retek <ext-mihaly.retek@nokia.com>
2009-02-24 11:49:41 +0200

    modify css

---
+++ dist/src/widget/mostusedcontainer.cpp
@@ -0,0 +1,63 @@
+
+#include "mostusedcontainer.h"
+
+#include "button2.h"
+
+#include <QtDebug>
+
+#include <duigridlayout.h>
+
+#include <duitheme.h>
+
+
+MostUsedContainer::MostUsedContainer() :
+    m_PosX(0) ,
+    m_PosY(0)
+
+{
+    m_Layout = new DuiGridLayout;
+
+    for (int xx =0; xx<2; xx++) {
+        m_Layout->setColumnMinimumWidth ( xx, layoutFieldWidth );
+        m_Layout->setColumnMaximumWidth ( xx, layoutFieldWidth );
+    }
+
+    for (int yy =0; yy<3; yy++) {
+        m_Layout->setRowMinimumHeight ( yy, layoutFieltHeight );
+        m_Layout->setRowMaximumHeight ( yy, layoutFieltHeight );
+    }
+
+}
+
+void MostUsedContainer::add(QString text1, QString text2, bool enable)
+{
+    //m_Layout->addItem(new DuiButton(text1), xx, yy, Qt::AlignCenter);
+    
+    //dummy data
+    int widthBig  = 350;
+    int heightBig = 100;
+    int posXSmall = 250;
+    int posYSmall = 10;
+    int widthSmall  = 80;
+    int heightSmall = 80;
+
+    CButton2 *tmpButton = new CButton2(text1, widthBig, heightBig, text2, posXSmall, posYSmall, widthSmall, heightSmall, enable);
+    //m_Layout->addItem(new DuiButton("test")/*tmpButton*/, m_PosX, m_PosY, Qt::AlignCenter);
+
+    m_Layout->addItem(tmpButton->layout(), m_PosY, m_PosX, Qt::AlignCenter);
+
+
+    m_PosX++;
+
+    if (m_PosX>1) {
+        m_PosX = 0;
+        m_PosY++;
+    }
+}
+
+
+DuiGridLayout* MostUsedContainer::layout()
+{
+    return m_Layout;
+}
+
