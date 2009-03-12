0773fefd798c7c2f852894183f7462a9090255ac
mihaly.retek <ext-mihaly.retek@nokia.com>
2009-03-12 11:59:54 +0200

    first

--- dist/src/main.cpp
+++ dist/src/main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
 {
     DuiApplication app(argc, argv);
 
-    DuiTheme::instance()->changeTheme("duicontrolpanel");
+  //  DuiTheme::instance()->changeTheme("duicontrolpanel");
   //  DuiTheme::loadCSS("duicontrolpanel.css");
 
     DuiLocale locale;                                                           
