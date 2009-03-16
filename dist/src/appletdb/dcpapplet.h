c9a94da517744a79415cb0fad935589ea0612d65
László Gál <lac@ubi.(none)>
2009-03-16 09:10:49 +0100

    missing dcpapplet.h added

---
+++ dist/src/appletdb/dcpapplet.h
@@ -0,0 +1,7 @@
+#ifndef DCPAPPLET_H
+#define DCPAPPLET_H
+namespace DcpApplet {
+    const QString DefaultPath = "/usr/lib/duicontrolpanel/";
+    const QString Lib = DefaultPath + "applets/";
+};
+#endif // DCPAPPLET_H
