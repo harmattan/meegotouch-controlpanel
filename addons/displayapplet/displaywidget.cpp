23dad287c3be351adfcc91506289157fbd7fcba7
Laszlo Rinyu <rinyu@rinyu.(none)>
2009-03-26 15:33:26 +0100

    Changed: DisplayApplet. Added ToggleButton, displayapplet.css.

--- addons/displayapplet/displaywidget.cpp
+++ addons/displayapplet/displaywidget.cpp
@@ -1,12 +1,18 @@
 #include "displaywidget.h"
 
+#include <duitheme.h>
+#include <duibutton.h>
 #include <duilinearlayout.h>
 #include <duilabel.h>
 #include <duislider.h>
 
+const QString cssDir = "/usr/share/themes/dui/duicontrolpanel/";
+const int widgetWidth = 100;
+
 DisplayWidget::DisplayWidget(QGraphicsWidget *parent)
 	    :DuiWidget(parent)
 {
+    DuiTheme::loadCSS(cssDir + "displayapplet.css");
 	initWidget();
 }
 
@@ -21,8 +27,8 @@ void DisplayWidget::paint(QPainter *painter,
 	Q_UNUSED(option);
 	Q_UNUSED(widget);
 
-    QPen pen(QColor(120, 120, 120, 255));
-    QBrush brush(QColor(50, 50, 50, 255));
+    QPen pen(QColor(120, 120, 120, 240));
+    QBrush brush(QColor(20, 20, 20, 240));
     painter->setPen(pen);
     painter->setBrush(brush);
     painter->drawRect(QRectF(0.0, 0.0,
@@ -32,52 +38,97 @@ void DisplayWidget::paint(QPainter *painter,
 
 void DisplayWidget::initWidget()
 {
-	this->setMinimumWidth(DuiDeviceProfile::instance()->width());
-	DuiLinearLayout *mainLayout = new DuiLinearLayout(Qt::Vertical, this);
-	mainLayout->setPadding(10);
+    DuiLinearLayout *mainLayout = new DuiLinearLayout(Qt::Horizontal, this);
+	
+    // leftWidget
+    DuiWidget *leftWidget  = new DuiWidget(this);
+    leftWidget->setMinimumWidth(widgetWidth);
+    leftWidget->setMaximumWidth(widgetWidth);
+    leftWidget->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Expanding);
+
+    // centralLayout
+    DuiLinearLayout *centralLayout = new DuiLinearLayout(Qt::Vertical, 0);
 	
 	m_brightnessLabel = new DuiLabel(QString("Brightness: %1 %").arg(0));
 	m_brightnessLabel->setObjectName("LabelBrightness");
-	mainLayout->addItem(m_brightnessLabel);
+	centralLayout->addItem(m_brightnessLabel);
 	
 	DuiSlider *sliderBrightness = new DuiSlider(this);
 	sliderBrightness->setOrientation(Qt::Horizontal);
 	sliderBrightness->setRange(0, 100);
-	sliderBrightness->setValue(0);
+	sliderBrightness->setValue(50);
 	sliderBrightness->setMaximumHeight(20);
 	connect(sliderBrightness, SIGNAL(valueChanged(int )), 
 		this, SLOT(setBrightnessLabel(int)));
-	mainLayout->addItem(sliderBrightness);
+	centralLayout->addItem(sliderBrightness);
 
 	m_screenLabel = new DuiLabel(QString("Screen lights on: %1 sec").arg(0));
-	mainLayout->addItem(m_screenLabel);
+    m_screenLabel->setObjectName("LabelScreen");
+	centralLayout->addItem(m_screenLabel);
 
 	DuiSlider *sliderScreen = new DuiSlider(this);
 	sliderScreen->setOrientation(Qt::Horizontal);
 	sliderScreen->setRange(0, 100);
-	sliderScreen->setValue(0);
+	sliderScreen->setValue(50);
 	sliderScreen->setMaximumHeight(20);
 	connect(sliderScreen, SIGNAL(valueChanged(int )),
 	 	this, SLOT(setScreenLabel(int )));
-	mainLayout->addItem(sliderScreen);
+	centralLayout->addItem(sliderScreen);
 
-	DuiWidget *plainWidget = new DuiWidget(this);
-	plainWidget->setMinimumHeight(7);
-	plainWidget->setMaximumHeight(7);
-	mainLayout->addItem(plainWidget);
+	DuiWidget *spacerItem = new DuiWidget(this);
+	spacerItem->setMinimumHeight(40);
+	spacerItem->setMaximumHeight(40);
+	centralLayout->addItem(spacerItem);
 
-	mainLayout->addItem(new DuiLabel("While charging keep screen lights"));
-	
-	DuiWidget *plainWidget2 = new DuiWidget(this);
-	plainWidget2->setMinimumHeight(7);
-	plainWidget2->setMaximumHeight(7);
-	mainLayout->addItem(plainWidget2);
+    // screenHLayout
+    DuiLinearLayout *screenHLayout = new DuiLinearLayout(Qt::Horizontal, 0);
+    screenHLayout->setSpacing(20);
 
-	mainLayout->addItem(new DuiLabel("Note! Display settings depend on the user power profile."));
+	DuiLabel *screenLightLabel = new DuiLabel("While charging keep screen lights", this);
+    screenLightLabel->setObjectName("LabelScreenLight");
+	screenHLayout->addItem(screenLightLabel);
 
-    DuiWidget *spacerItem = new DuiWidget(this);
-    spacerItem->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
-    mainLayout->addItem(spacerItem);
+    m_screenToggleButton = new DuiButton(this);
+    m_screenToggleButton->setObjectName("ScreenToggleButton");
+    m_screenToggleButton->setCheckable(true);
+    screenHLayout->addItem(m_screenToggleButton);
+
+    DuiWidget *spacerItem2 = new DuiWidget(this);
+    spacerItem2->setMaximumHeight(20);
+    spacerItem2->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
+    screenHLayout->addItem(spacerItem2);
+
+    screenHLayout->setAlignment(screenLightLabel, Qt::AlignLeft | Qt::AlignVCenter);
+    screenHLayout->setAlignment(m_screenToggleButton, Qt::AlignLeft | Qt::AlignVCenter);
+
+    centralLayout->addItem(screenHLayout);
+    
+	DuiWidget *spacerItem3 = new DuiWidget(this);
+	spacerItem3->setMinimumHeight(30);
+	spacerItem3->setMaximumHeight(30);
+	centralLayout->addItem(spacerItem3);
+
+	centralLayout->addItem(new DuiLabel("Note! Display settings depend on the user power profile."));
+
+    DuiWidget *spacerItem4 = new DuiWidget(this);
+    spacerItem4->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+    centralLayout->addItem(spacerItem4);
+
+    centralLayout->setAlignment(m_brightnessLabel, Qt::AlignLeft | Qt::AlignVCenter);
+
+    // rigthWidget
+    DuiWidget *rightWidget  = new DuiWidget(this);
+    rightWidget->setMinimumWidth(widgetWidth);
+    rightWidget->setMaximumWidth(widgetWidth);
+    rightWidget->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Expanding);
+
+    mainLayout->addItem(leftWidget);
+    mainLayout->addItem(centralLayout);
+    mainLayout->addItem(rightWidget);
+
+    mainLayout->setAlignment(leftWidget, Qt::AlignLeft);
+    mainLayout->setAlignment(centralLayout, Qt::AlignHCenter);
+    mainLayout->setAlignment(rightWidget, Qt::AlignRight);
 }
 
 void DisplayWidget::setBrightnessLabel(int value)
