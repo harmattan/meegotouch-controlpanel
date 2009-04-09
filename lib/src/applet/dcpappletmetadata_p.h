1840925f3d400092e861bfc2ba58cfb47e078aca
László Gál <lac@ubi.(none)>
2009-04-09 08:58:30 +0200

    missing files

---
+++ lib/src/applet/dcpappletmetadata_p.h
@@ -0,0 +1,9 @@
+#ifndef DCPAPPLETMETADATA_P_H
+#define DCPAPPLETMETADATA_P_H
+#include <QFileInfo>
+class DcpAppletMetadataPrivate
+{
+public:
+    QFileInfo m_FileInfo;
+};
+#endif // DCPAPPLETMETADATA_P_H
