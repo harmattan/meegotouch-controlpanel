d36ada35d20b3b7efedfc699d22269af6860131c
László Gál <lac@ubi.(none)>
2009-04-17 12:53:57 +0200

    sprint backlog W17

--- dist/src/base/maincategory/dcpmaincategory.h
+++ dist/src/base/maincategory/dcpmaincategory.h
@@ -3,6 +3,7 @@
 
 #include "dcpcategory.h"
 #include <QMap>
+class DuiLayout;
 class DcpMainCategory : public DcpCategory
 {
     Q_OBJECT
@@ -14,14 +15,14 @@ public:
     virtual void add(DcpComponent *component);
     virtual void append(DcpComponent *component);
     virtual void setMaxColumns(int columns){m_MaxColumns = columns;};
-                                                                               
+    DuiLayout* duiLayout() const {return m_Layout;};                                                                             
 public slots:                                                                   
     virtual void onOrientationChange (const Dui::Orientation &orientation);   
 protected:
     virtual void createContents();
 
 protected:
-    class DuiLayout* m_Layout;
+    DuiLayout* m_Layout;
     class DuiGridLayoutPolicy* m_LandscapeLayout;
     class DuiLinearLayoutPolicy* m_PortraitLayout;
 
