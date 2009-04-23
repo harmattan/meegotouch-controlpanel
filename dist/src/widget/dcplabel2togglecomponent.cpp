695c930494dee8e4e69b0a0da077707b5fdd6325
mihaly.retek <ext-mihaly.retek@nokia.com>
2009-04-23 13:33:52 +0200

    toggle bigfix

--- dist/src/widget/dcplabel2togglecomponent.cpp
+++ dist/src/widget/dcplabel2togglecomponent.cpp
@@ -14,7 +14,8 @@ DcpLabel2ToggleComponent::DcpLabel2ToggleComponent( DcpCategory *category,
                                                     DcpAppletMetadata * metadata,
                                                     const QString& title,
                                                     QGraphicsWidget *parent):
-                          DcpBasicComponent(category, metadata, title, parent)
+                          DcpBasicComponent(category, metadata, title, parent),
+   m_Alignment(metadata->align())
 {
     m_Type = DCPLABEL2BUTTON;
     m_EnableSmallToggle = metadata->smallToggle();
@@ -30,8 +31,14 @@ DcpLabel2ToggleComponent::~DcpLabel2ToggleComponent()
 void DcpLabel2ToggleComponent::createContents()
 {
 
-
-		m_Button = new DcpButton(DCPLABEL2TOGGLE);
+	if ( m_Alignment==Qt::AlignLeft ) {
+        m_Button = new DcpButton(DCPLABEL2TOGGLE);
+				m_Button->setAlignment(Qt::AlignLeft);
+		}
+    else {
+        m_Button = new DcpButton(DCPLABEL2TOGGLE);
+				m_Button->setAlignment(Qt::AlignRight);
+		}
 
 		m_Button->setText(metadata()->text1(), metadata()->text2());
    	m_WidgetLayoutPolicy->addItemAtPosition(m_Button, 0, 0);
--- dist/src/widget/dcplabel2togglecomponent.h
+++ dist/src/widget/dcplabel2togglecomponent.h
@@ -40,6 +40,8 @@ protected:
     bool m_EnableSmallToggle;
 
 		DcpButton *m_Button;
+
+    Qt::Alignment m_Alignment;
 };
 
 
