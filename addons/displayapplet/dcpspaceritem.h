7fcab30a707ed2ff9f1a2b760ad065c636fe00fc
Laszlo Rinyu <laszlo.rinyu@blumsfot.eu>
2009-04-23 11:03:54 +0200

    Upgraded: DisplayApplet.

---
+++ addons/displayapplet/dcpspaceritem.h
@@ -0,0 +1,18 @@
+#ifndef DCPSPACERITEM_H
+#define DCPSPACERITEM_H
+
+#include <DuiWidget>
+
+class DcpSpacerItem : public DuiWidget
+{
+public:
+    DcpSpacerItem(DuiWidget *parent, int width, int height, 
+                  QSizePolicy::Policy hPolicy = QSizePolicy::Minimum,
+                  QSizePolicy::Policy vPolicy = QSizePolicy::Minimum);
+    virtual ~DcpSpacerItem();
+
+    void changeSize(int width, int height,
+                    QSizePolicy::Policy hPolicy = QSizePolicy::Minimum,
+                    QSizePolicy::Policy vPolicy = QSizePolicy::Minimum);
+};
+#endif // DCPSPACERITEM_H
