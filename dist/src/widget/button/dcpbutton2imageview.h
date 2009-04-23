f56ef55ebd1a044fab52c648ded8dc133563702e
mihaly.retek <ext-mihaly.retek@nokia.com>
2009-04-23 10:08:28 +0200

    merge

---
+++ dist/src/widget/button/dcpbutton2imageview.h
@@ -0,0 +1,31 @@
+
+#ifndef DCPBUTTON2IMAGEVIEW_H
+#define DCPBUTTON2IMAGEVIEW_H
+
+#include <DuiWidgetView>
+#include "dcpbutton.h"
+
+#include "dcpbutton2view.h"
+
+class QPainter;
+
+class DcpButton2ImageView : public DcpButton2View
+{
+    Q_OBJECT
+    DUI_STYLABLE
+
+public:
+    DcpButton2ImageView(DcpButton &controller);
+
+    virtual ~DcpButton2ImageView();
+
+    virtual void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget = NULL);
+
+		virtual void paintTextLeft(QPainter *painter);
+		virtual void paintImageLeft(QPainter *painter);
+
+private:
+    Q_DISABLE_COPY(DcpButton2ImageView);
+};
+
+#endif
