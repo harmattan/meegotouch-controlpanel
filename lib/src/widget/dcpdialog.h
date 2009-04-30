38463577396cf7dc0dd711920a162e3939a506e6
László Gál <laszlo.gal@blumsoft.eu>
2009-04-30 08:41:00 +0200

    language app: message box appears when no keyboard languages selected and user press back

---
+++ lib/src/widget/dcpdialog.h
@@ -0,0 +1,13 @@
+#ifndef DCPDIALOG_H
+#define DCPDIALOG_H
+#include <DuiDialog>
+
+class DcpDialog : public DuiDialog
+{
+public:
+    DcpDialog(); 
+    virtual ~DcpDialog(); 
+    virtual void close();
+};
+
+#endif // DCPDIALOG_H
