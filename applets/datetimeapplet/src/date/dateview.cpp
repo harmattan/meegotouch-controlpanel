db93713f3e590169b7f72aedb0cf1c823caed17e
retsoft <retsoft@ubuntu.(none)>
2009-09-21 10:35:36 +0200

    implement original datapicker

--- applets/datetimeapplet/src/date/dateview.cpp
+++ applets/datetimeapplet/src/date/dateview.cpp
@@ -2,15 +2,11 @@
 #include "dcpdatetime.h"
 #include "datetimetranslation.h"
 #include "dcpspaceritem.h"
-#include "stepbutton.h"
-#include "datepicker.h"
 
 #include <DuiWidget>
 #include <duicontainer.h>
 #include <duilabel.h>
 #include <duiscenemanager.h>
-#include <duicalendar.h>
-#include <QDateTime>
 #include <QGraphicsLinearLayout>
 
 
@@ -46,94 +42,17 @@ void DateView::initWidget()
     widgetLayout->setContentsMargins(0.0, 0.0, 0.0, 0.0);
     widgetLayout->setSpacing(1);
 
-/*
-    // labelLayout
-    QGraphicsLinearLayout *labelLayout = new QGraphicsLinearLayout(Qt::Horizontal, 0);
-    labelLayout->setContentsMargins(0.0, 0.0, 0.0, 0.0);
-    labelLayout->setSpacing(1);
-
-    // labelVLayout
-    QGraphicsLinearLayout *labelVLayout = new QGraphicsLinearLayout(Qt::Vertical, 0);
-    labelVLayout->setContentsMargins(0.0, 0.0, 0.0, 0.0);
-    labelVLayout->setSpacing(1);
-
-    // pickerLayout
-    QGraphicsLinearLayout *pickerLayout = new QGraphicsLinearLayout(Qt::Vertical, 0);
-    pickerLayout->setContentsMargins(0.0, 0.0, 0.0, 0.0);
-    pickerLayout->setSpacing(1);
-
-    // leftStepButton
-    StepButton *leftStepButton = new StepButton(StepButton::Left, this);
-    leftStepButton->setMinimumSize(QSize(20, 40));
-    leftStepButton->setMaximumSize(QSize(20, 40));
-
-    // rightStepButton
-    StepButton *rightStepButton = new StepButton(StepButton::Right, this);
-    rightStepButton->setMinimumSize(QSize(20, 40));
-    rightStepButton->setMaximumSize(QSize(20, 40));
-
-    // m_DateLabel
-    m_DateLabel = new DuiLabel(QDateTime::currentDateTime().toString("MMM dd"), this);
-    m_DateLabel->setObjectName("DateViewLabel");
-    m_DateLabel->setAlignment(Qt::AlignCenter);
-
-    DuiCalendar calendar;
-    calendar.setDateTime(QDateTime::currentDateTime());
-    
-    // m_WeekLabel
-    m_WeekLabel = new DuiLabel("Week " + QString::number(calendar.weekOfYear()), this);
-    m_WeekLabel->setObjectName("WeekViewLabel");
-    m_WeekLabel->setAlignment(Qt::AlignCenter);
-
-    // datePicker
-    datePicker = new DatePicker(this);
-    connect(leftStepButton, SIGNAL(clicked()), datePicker, SLOT(left()));
-    connect(rightStepButton, SIGNAL(clicked()), datePicker, SLOT(right()));
-
-
-	connect(leftStepButton, SIGNAL(clicked()), this, SLOT(modifyDate()));
-    connect(rightStepButton, SIGNAL(clicked()), this, SLOT(modifyDate()));
-
-	connect(datePicker, SIGNAL(keySignal(QString, QString)), this, SLOT(keySlot(QString, QString)));
-
-    // add items to labelVLayoutPolicy
-    labelVLayout->addItem(m_DateLabel);
-    labelVLayout->setAlignment(m_DateLabel, Qt::AlignCenter);
-    labelVLayout->addItem(m_WeekLabel);
-    labelVLayout->setAlignment(m_WeekLabel, Qt::AlignCenter);
-
-
-    // add items to labelLayoutPolicy
-    labelLayout->addItem(leftStepButton);
-    labelLayout->setAlignment(leftStepButton, Qt::AlignCenter);
-    labelLayout->addItQPushButtonem(new DcpSpacerItem(this, 5, 5, QSizePolicy::Expanding, QSizePolicy::Fixed));
-    labelLayout->addItem(labelVLayout);
-    labelLayout->addItem(new DcpSpacerItem(this, 5, 5, QSizePolicy::Expanding ,QSizePolicy::Fixed));
-    labelLayout->addItem(rightStepButton);
-    labelLayout->setAlignment(rightStepButton, Qt::AlignCenter);
-    
-    // add items to pickerLayout
-    pickerLayout->addItem(labelLayout);
-    pickerLayout->addItem(datePicker);
-    
-    // add items to widgetLayoutPolicy
-    widgetLayout->addItem(new DcpSpacerItem(this, 5, 5, QSizePolicy::Expanding, QSizePolicy::Preferred));
-    widgetLayout->addItem(pickerLayout);
-    widgetLayout->addItem(new DcpSpacerItem(this, 5, 5, QSizePolicy::Expanding, QSizePolicy::Preferred));
-*/  
-
-
   	//widgetLayout->addItem(pickerLayout);
 
     // setCentralWidget
 
-	MonthWidget* tmp = new MonthWidget();
+	m_MonthWidget = new MonthWidget();
 
-	MonthWidgetFullView* tmpView = new MonthWidgetFullView(tmp);
+	MonthWidgetFullView* tmpView = new MonthWidgetFullView(m_MonthWidget);
 
-	tmp->setView(tmpView);
+	m_MonthWidget->setView(tmpView);
 
-    m_Container->setCentralWidget(tmp);
+    m_Container->setCentralWidget(m_MonthWidget);
 
     // add items to mainLayoutPolicy
     mainLayout->addItem(m_Container);
@@ -141,15 +60,4 @@ void DateView::initWidget()
 
 }
 
-void DateView::modifyDate()
-{
-	m_DateLabel->setText(datePicker->day());
-	m_WeekLabel->setText(" ----- ");
-}
-
-void DateView::keySlot(QString day, QString week)
-{
-	m_DateLabel->setText(day);
-	m_WeekLabel->setText(week);
-}
 
