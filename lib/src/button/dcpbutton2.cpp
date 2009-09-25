63a87a42696ecbc1959e8075f883f5c2fddb8ad8
manner <robert.manner@blumsoft.eu>
2009-09-25 13:19:26 +0200

    Adds objectname for dcpbutton2 line 1
    Details:
    needed for testing

--- lib/src/button/dcpbutton2.cpp
+++ lib/src/button/dcpbutton2.cpp
@@ -31,6 +31,7 @@ QGraphicsLayout* DcpButton2::createLayout()
     // create the labels:
     m_Label1 = new DuiLabel (this);
     m_Label1->setAcceptedMouseButtons(0);
+    m_Label2->setObjectName("DcpButtonLine1");
 //    m_Label1->setSizePolicy(QSizePolicy::Expanding,QSizePolicy::Expanding);
 
     m_Label2 = new DuiLabel();
