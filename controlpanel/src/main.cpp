f5d497c9bf92fb4fe7be20f72c7e53653341d495
manner <robert.manner@blumsoft.eu>
2009-10-12 15:27:42 +0200

    (DCP) prefix for debug messages

--- controlpanel/src/main.cpp
+++ controlpanel/src/main.cpp
@@ -1,14 +1,38 @@
+#include "mainwindow.h"
+#include "service/duicontrolpanelservice.h"
 
 #include <DuiApplication>
 #include <DuiLocale>
 #include <QtDebug>
+#include <stdio.h>
+#include <stdlib.h>
 
-#include "mainwindow.h"
-#include "service/duicontrolpanelservice.h"
+static const char* DEBUG_PREFIX = "(DCP) ";
+
+void dcpMsg(QtMsgType type, const char *msg)
+{
+    switch (type) {
+        case QtDebugMsg:
+            fprintf(stderr, "%s%s\n", DEBUG_PREFIX, msg);
+            break;
+        case QtWarningMsg:
+            fprintf(stderr, "%s* %s *\n", DEBUG_PREFIX, msg);
+            break;
+        case QtCriticalMsg:
+            fprintf(stderr, "%s** %s **\n", DEBUG_PREFIX, msg);
+            break;
+        case QtFatalMsg:
+            fprintf(stderr, "Fatal: %s%s\n", DEBUG_PREFIX, msg);
+            abort();
+    }
+}
 
 int main(int argc, char *argv[])
 {
+    qInstallMsgHandler(dcpMsg);
+
     DuiControlPanelService* service = new DuiControlPanelService();
+
     DuiApplication app(argc, argv);
     app.setAnimator(0);
 
