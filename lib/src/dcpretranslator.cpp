1b4a79377d4e3868155d638d829d4e3d11734893
manner <robert.manner@blumsoft.eu>
2009-10-26 12:37:47 +0100

    Makes DcpRetranslator reusable
    Details:
    duistartup needs exactly the same functionality

--- lib/src/dcpretranslator.cpp
+++ lib/src/dcpretranslator.cpp
@@ -2,6 +2,7 @@
 #include <DuiLocale>
 #include <QtDebug>
 #include <DuiGConfItem>
+#include <DuiApplication>
 
 // copied from widgetsgallery, makes translation reload automatically
 void 
@@ -11,7 +12,7 @@ Retranslator::retranslate()
     DuiGConfItem languageItem("/Dui/i18n/Language");
     QString language = languageItem.value().toString();
     DuiLocale locale(language);
-    locale.installCategoryCatalog(DuiLocale::DuiLcMessages, "duicontrolpanel");
+    locale.installCategoryCatalog(DuiLocale::DuiLcMessages, qApp->applicationName());
     DuiLocale::setDefault(locale);
 }
 
