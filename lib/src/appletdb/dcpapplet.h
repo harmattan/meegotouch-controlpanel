30f142890838269dc09969c33a6689f621d23383
laszlo.gal <laszlo.gal@blumsoft.eu>
2009-11-09 16:20:16 +0100

    DcpApplet::MaxMostUsed public constant

--- lib/src/appletdb/dcpapplet.h
+++ lib/src/appletdb/dcpapplet.h
@@ -4,5 +4,6 @@ namespace DcpApplet {
     const QString DefaultPath = DESKTOP_DIR;
     const QString Lib = APPLET_LIBS;
     const QString MostUsedCategory = "MostUsedCategory";
+    const int MaxMostUsed = 6;
 };
 #endif // DCPAPPLET_H
