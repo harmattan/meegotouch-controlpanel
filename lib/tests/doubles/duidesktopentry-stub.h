9c93bb4a667b5d53d3a4484d0d30fb1a05b90895
Viktor Nagy <viktor.nagy@blumsoft.eu>
2009-12-08 09:53:10 +0100

    added forgotten file

---
+++ lib/tests/doubles/duidesktopentry-stub.h
@@ -0,0 +1,44 @@
+#ifndef DUIDESKTOPENTRY_STUB_H__
+#define DUIDESKTOPENTRY_STUB_H__
+
+#include <DuiDesktopEntry>
+
+DuiDesktopEntry::DuiDesktopEntry(const QString &filename) : d_ptr(0)
+{
+    Q_UNUSED(filename);
+}
+
+DuiDesktopEntry::~DuiDesktopEntry()
+{
+}
+
+QString  DuiDesktopEntry::fileName () const { return ""; }
+bool  DuiDesktopEntry::isValid () const { return true; }
+uint  DuiDesktopEntry::hash () const { return 0; }
+QString  DuiDesktopEntry::type () const { return ""; }
+QString  DuiDesktopEntry::version () const { return ""; }
+QString  DuiDesktopEntry::name () const { return ""; }
+QString  DuiDesktopEntry::nameUnlocalized () const { return ""; }
+QString  DuiDesktopEntry::genericName () const { return ""; }
+bool  DuiDesktopEntry::noDisplay () const { return false; }
+QString  DuiDesktopEntry::comment () const { return ""; }
+QString  DuiDesktopEntry::icon () const { return ""; }
+bool  DuiDesktopEntry::hidden () const { return false; }
+QStringList  DuiDesktopEntry::onlyShowIn () const { QStringList l; return l; }
+QStringList  DuiDesktopEntry::notShowIn () const { QStringList l; return l; }
+QString  DuiDesktopEntry::tryExec () const { return ""; }
+QString  DuiDesktopEntry::exec () const { return ""; }
+QString  DuiDesktopEntry::path () const { return ""; }
+bool  DuiDesktopEntry::terminal () const { return false; }
+QStringList  DuiDesktopEntry::mimeType () const { QStringList l; return l; }
+QStringList  DuiDesktopEntry::categories () const { QStringList l; return l; }
+bool  DuiDesktopEntry::startupNotify () const { return false; }
+QString  DuiDesktopEntry::startupWMClass () const { return ""; }
+QString  DuiDesktopEntry::url () const { return ""; }
+QString  DuiDesktopEntry::xMaemoService () const { return ""; }
+QString  DuiDesktopEntry::value (QString) const { return ""; }
+QString  DuiDesktopEntry::value (QString, QString) const { return ""; }
+bool  DuiDesktopEntry::contains (QString) const { return false; }
+bool  DuiDesktopEntry::contains (QString, QString) const  { return false; }
+#endif
+
