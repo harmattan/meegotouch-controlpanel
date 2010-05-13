a4186362c0fe111395fb3a536722dd30d798ebc2
Pinter Sandor <sandor.pinter@blumsoft.eu>
2010-05-13 13:57:47 +0200

    Adding LGPL headers

--- declapplet/dcpdeclwidget.cpp
+++ declapplet/dcpdeclwidget.cpp
@@ -1,3 +1,20 @@
+/***************************************************************************
+**
+** Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies).
+** All rights reserved.
+** Contact: Karoliina T. Salminen <karoliina.t.salminen@nokia.com>
+**
+** This file is part of duicontrolpanel.
+**
+**
+** This library is free software; you can redistribute it and/or
+** modify it under the terms of the GNU Lesser General Public
+** License version 2.1 as published by the Free Software Foundation
+** and appearing in the file LICENSE.LGPL included in the packaging
+** of this file.
+**
+****************************************************************************/
+
 #include "dcpdeclwidget.h"
 
 #include <QGraphicsLinearLayout>
